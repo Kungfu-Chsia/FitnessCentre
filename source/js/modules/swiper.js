ee489a7c29327215a59b2993c25fb35c7974b56b
Kungfu-Chsia <allrighty@ya.ru>
2022-06-07 20:22:16 +0300

    swiper

--- source/js/modules/swiper.js
+++ source/js/modules/swiper.js
@@ -1,54 +1,50 @@
 const initSwiper = function () {
 
-  // const swiper = new Swiper('.trainers__swiper', {
+  const swiper = new Swiper('.trainers__swiper', {
 
-  //   // Стрелки
-  //   navigation: {
-  //     nextEl: '.trainers__button-next',
-  //     prevEl: '.trainers__button-prev',
-  //   },
+    navigation: {
+      nextEl: '.trainers__button-next',
+      prevEl: '.trainers__button-prev',
+    },
 
-  //   // Отключаем скрол на ПК
-  //   simulateTouch: false,
+    // отключение скролла
+    simulateTouch: false,
 
-  //   // Бесконечная прокрутка
-  //   loop: true,
+    // Бесконечная прокрутка
+    loop: true,
 
-  //   // Брейк поинты (адаптив)
-  //   // Ширина экрана
-  //   breakpoints: {
-  //     320: {
-  //       // Колличество слайдов на странице
-  //       slidesPerView: 1,
-  //       // С какого слайда начинать
-  //       initialSlide: 2,
-  //     },
-  //     768: {
-  //       // Колличество слайдов на странице
-  //       slidesPerView: 2,
-  //       // Отступ между слайдами
-  //       spaceBetween: 30,
-  //       // С какого слайда начинать
-  //       initialSlide: 2,
-  //     },
-  //     1200: {
-  //       // Колличество слайдов на странице
-  //       slidesPerView: 3,
-  //       // Отступ между слайдами
-  //       spaceBetween: 40,
-  //       // С какого слайда начинать
-  //       initialSlide: 0,
-  //     },
-  //     1366: {
-  //       // Колличество слайдов на странице
-  //       slidesPerView: 4,
-  //       // Отступ между слайдами
-  //       spaceBetween: 40,
-  //       // С какого слайда начинать
-  //       initialSlide: 0,
-  //     },
-  //   },
-  // });
+    breakpoints: {
+      320: {
+        // Количество слайдов на странице
+        slidesPerView: 1,
+        // С какого слайда начинать
+        initialSlide: 2,
+      },
+      768: {
+        slidesPerView: 2,
+        // Отступ между слайдами
+        spaceBetween: 30,
+        // С какого слайда начинать
+        initialSlide: 2,
+      },
+      1200: {
+        // Колличество слайдов на странице
+        slidesPerView: 3,
+        // Отступ между слайдами
+        spaceBetween: 40,
+        // С какого слайда начинать
+        initialSlide: 0,
+      },
+      1366: {
+        // Колличество слайдов на странице
+        slidesPerView: 4,
+        // Отступ между слайдами
+        spaceBetween: 40,
+        // С какого слайда начинать
+        initialSlide: 0,
+      },
+    },
+  });
 
   // const swiperFeedback = new Swiper('.feedback-swiper', {
 
